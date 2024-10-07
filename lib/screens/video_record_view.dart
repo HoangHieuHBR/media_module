ef584d9f726be2423a93465846c3c46dc64785a5
Hoang Hieu <dinhhoanghieu.hanbiro@gmail.com>
2024-10-07 17:07:32 +0700

    feat: :sparkles: Update UI and record function (In Progress)

--- lib/screens/video_record_view.dart
+++ lib/screens/video_record_view.dart
@@ -1,6 +1,7 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:video_player/video_player.dart';
 
 class VideoRecordView extends StatefulWidget {
@@ -21,6 +22,8 @@ class _VideoRecordViewState extends State<VideoRecordView> {
 
   XFile? imageFile;
   XFile? videoFile;
+
+  late Stopwatch _stopwatch;
 
   List<CameraDescription> _cameras = <CameraDescription>[];
 
@@ -44,10 +47,7 @@ class _VideoRecordViewState extends State<VideoRecordView> {
 
     _initCamera();
 
-    if (_cameras.isNotEmpty) {
-      _selectedCamera = _cameras.first;
-      _initializeCameraController(_selectedCamera!);
-    }
+    _stopwatch = Stopwatch();
   }
 
   @override
@@ -58,6 +58,11 @@ class _VideoRecordViewState extends State<VideoRecordView> {
 
   Future<void> _initCamera() async {
     _cameras = await availableCameras();
+
+    if (_cameras.isNotEmpty) {
+      _selectedCamera = _cameras.first;
+      _initializeCameraController(_selectedCamera!);
+    }
   }
 
   Future<void> _initializeCameraController(
@@ -130,20 +135,6 @@ class _VideoRecordViewState extends State<VideoRecordView> {
     }
   }
 
-  void _showInSnackBar(String message) {
-    ScaffoldMessenger.of(context)
-        .showSnackBar(SnackBar(content: Text(message)));
-  }
-
-  void _showCameraException(CameraException e) {
-    _logError(e.code, e.description);
-    _showInSnackBar('Error: ${e.code}\n${e.description}');
-  }
-
-  void _handleScaleStart(ScaleStartDetails details) {
-    _baseScale = _currentScale;
-  }
-
   Future<void> _handleScaleUpdate(ScaleUpdateDetails details) async {
     // When there are not exactly two fingers on screen don't scale
     if (controller == null || _pointers != 2) {
@@ -156,21 +147,6 @@ class _VideoRecordViewState extends State<VideoRecordView> {
     await controller!.setZoomLevel(_currentScale);
   }
 
-  void onViewFinderTap(TapDownDetails details, BoxConstraints constraints) {
-    if (controller == null) {
-      return;
-    }
-
-    final CameraController cameraController = controller!;
-
-    final Offset offset = Offset(
-      details.localPosition.dx / constraints.maxWidth,
-      details.localPosition.dy / constraints.maxHeight,
-    );
-    cameraController.setExposurePoint(offset);
-    cameraController.setFocusPoint(offset);
-  }
-
   Future<void> _onNewCameraSelected(CameraDescription cameraDescription) async {
     await _initializeCameraController(cameraDescription);
     setState(() {
@@ -178,19 +154,75 @@ class _VideoRecordViewState extends State<VideoRecordView> {
     });
   }
 
-  void _onTakePictureButtonPressed() {
-    _takePicture().then((XFile? file) {
-      if (mounted) {
-        setState(() {
-          imageFile = file;
-          videoController?.dispose();
-          videoController = null;
-        });
-        if (file != null) {
-          _showInSnackBar('Picture saved to ${file.path}');
-        }
+  Future<void> _startVideoRecording() async {
+    final CameraController? cameraController = controller;
+
+    if (cameraController == null || !cameraController.value.isInitialized) {
+      _showInSnackBar('Error: select a camera first.');
+      return;
+    }
+
+    if (cameraController.value.isRecordingVideo) {
+      // A recording is already started, do nothing.
+      return;
+    }
+
+    try {
+      await cameraController.startVideoRecording();
+      // Stop recording after 5 minutes
+      if (cameraController.value.isRecordingVideo &&
+          _stopwatch.elapsed.inSeconds >= 300) {
+        _onStopButtonPressed();
       }
-    });
+    } on CameraException catch (e) {
+      _showCameraException(e);
+      return;
+    }
+  }
+
+  Future<void> _pauseVideoRecording() async {
+    final CameraController? cameraController = controller;
+
+    if (cameraController == null || !cameraController.value.isRecordingVideo) {
+      return;
+    }
+
+    try {
+      await cameraController.pauseVideoRecording();
+    } on CameraException catch (e) {
+      _showCameraException(e);
+      rethrow;
+    }
+  }
+
+  Future<void> _resumeVideoRecording() async {
+    final CameraController? cameraController = controller;
+
+    if (cameraController == null || !cameraController.value.isRecordingVideo) {
+      return;
+    }
+
+    try {
+      await cameraController.resumeVideoRecording();
+    } on CameraException catch (e) {
+      _showCameraException(e);
+      rethrow;
+    }
+  }
+
+  Future<XFile?> _stopVideoRecording() async {
+    final CameraController? cameraController = controller;
+
+    if (cameraController == null || !cameraController.value.isRecordingVideo) {
+      return null;
+    }
+
+    try {
+      return cameraController.stopVideoRecording();
+    } on CameraException catch (e) {
+      _showCameraException(e);
+      return null;
+    }
   }
 
   Future<XFile?> _takePicture() async {
@@ -214,17 +246,118 @@ class _VideoRecordViewState extends State<VideoRecordView> {
     }
   }
 
+  void _showInSnackBar(String message) {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
+  }
+
+  void _showCameraException(CameraException e) {
+    _logError(e.code, e.description);
+    _showInSnackBar('Error: ${e.code}\n${e.description}');
+  }
+
+  void _handleScaleStart(ScaleStartDetails details) {
+    _baseScale = _currentScale;
+  }
+
+  void onViewFinderTap(TapDownDetails details, BoxConstraints constraints) {
+    if (controller == null) {
+      return;
+    }
+
+    final CameraController cameraController = controller!;
+
+    final Offset offset = Offset(
+      details.localPosition.dx / constraints.maxWidth,
+      details.localPosition.dy / constraints.maxHeight,
+    );
+    cameraController.setExposurePoint(offset);
+    cameraController.setFocusPoint(offset);
+  }
+
+  void _onTakePictureButtonPressed() {
+    _takePicture().then((XFile? file) {
+      if (mounted) {
+        setState(() {
+          imageFile = file;
+          videoController?.dispose();
+          videoController = null;
+        });
+        if (file != null) {
+          _showInSnackBar('Picture saved to ${file.path}');
+        }
+      }
+    });
+  }
+
+  void _onVideoRecordButtonPressed() {
+    _startVideoRecording().then((_) {
+      _handleStartStop();
+      if (mounted) {
+        setState(() {});
+      }
+    });
+  }
+
+  void _onStopButtonPressed() {
+    _stopVideoRecording().then((XFile? file) {
+      _timerReset();
+      if (mounted) {
+        setState(() {});
+      }
+      if (file != null) {
+        _showInSnackBar('Video recorded to ${file.path}');
+        videoFile = file;
+        // _startVideoPlayer();
+      }
+    });
+  }
+
+  void _onPauseButtonPressed() {
+    _pauseVideoRecording().then((_) {
+      _handleStartStop();
+      if (mounted) {
+        setState(() {});
+      }
+      _showInSnackBar('Video recording paused');
+    });
+  }
+
+  void _onResumeButtonPressed() {
+    _resumeVideoRecording().then((_) {
+      _handleStartStop();
+      if (mounted) {
+        setState(() {});
+      }
+      _showInSnackBar('Video recording resumed');
+    });
+  }
+
+  void _handleStartStop() {
+    if (_stopwatch.isRunning) {
+      _stopwatch.stop();
+    } else {
+      _stopwatch.start();
+    }
+    setState(() {});
+  }
+
+  void _timerReset() {
+    if (_stopwatch.isRunning) {
+      setState(() {
+        _stopwatch.reset();
+      });
+    }
+  }
+
   Widget buildCameraPreviewWidget() {
     final CameraController? cameraController = controller;
 
     if (cameraController == null || !cameraController.value.isInitialized) {
-      return const Text(
-        'Tap a camera',
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 24.0,
-          fontWeight: FontWeight.w900,
-        ),
+      return Container(
+        height: MediaQuery.sizeOf(context).height,
+        width: MediaQuery.sizeOf(context).width,
+        color: Colors.grey,
       );
     } else {
       return Listener(
@@ -248,6 +381,8 @@ class _VideoRecordViewState extends State<VideoRecordView> {
   }
 
   Widget buildCameraActionButton() {
+    final CameraController? cameraController = controller;
+
     void onChanged() {
       if (_cameras.isEmpty) {
         return;
@@ -259,12 +394,89 @@ class _VideoRecordViewState extends State<VideoRecordView> {
     }
 
     Widget buildCaptureButton({bool isTakePhotoMode = false}) {
+      if (cameraController != null) {
+        if (cameraController.value.isRecordingVideo) {
+          return StreamBuilder<int>(
+            stream: Stream.periodic(const Duration(seconds: 1),
+                (_) => _stopwatch.elapsed.inSeconds).asBroadcastStream(),
+            builder: (context, snapshot) {
+              final elapsedSeconds = snapshot.data ?? 0;
+              final minutes = (elapsedSeconds ~/ 60).toString().padLeft(1, '0');
+              final seconds = (elapsedSeconds % 60).toString().padLeft(2, '0');
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    height: 20,
+                    width: 70,
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.onSurfaceVariant,
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(20),
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "$minutes:$seconds / 5:00",
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodySmall
+                            ?.copyWith(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  ElevatedButton(
+                    onPressed: _onStopButtonPressed,
+                    style: ElevatedButton.styleFrom(
+                      shape: const CircleBorder(
+                        side: BorderSide(
+                          color: Colors.white,
+                          width: 2.0,
+                        ),
+                      ),
+                      padding: const EdgeInsets.all(20),
+                      backgroundColor: Colors.transparent,
+                    ),
+                    child: const Center(
+                      child: Icon(
+                        Icons.stop,
+                        color: Colors.red,
+                        size: 35,
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            },
+          );
+        }
+        return ElevatedButton(
+          onPressed: () {
+            if (isTakePhotoMode && !cameraController.value.isRecordingVideo) {
+              _onTakePictureButtonPressed();
+            } else if (!isTakePhotoMode &&
+                !cameraController.value.isRecordingVideo) {
+              _onVideoRecordButtonPressed();
+            }
+          },
+          child: null,
+          style: ElevatedButton.styleFrom(
+            shape: const CircleBorder(
+              side: BorderSide(
+                color: Colors.white,
+                width: 2.0,
+              ),
+            ),
+            padding: const EdgeInsets.all(35),
+            backgroundColor: isTakePhotoMode ? Colors.grey : Colors.red,
+          ),
+        );
+      }
       return ElevatedButton(
-        onPressed: () {
-          if (isTakePhotoMode) {
-            _onTakePictureButtonPressed();
-          }
-        },
+        onPressed: null,
         child: null,
         style: ElevatedButton.styleFrom(
           shape: const CircleBorder(
@@ -274,20 +486,54 @@ class _VideoRecordViewState extends State<VideoRecordView> {
             ),
           ),
           padding: const EdgeInsets.all(35),
-          backgroundColor: isTakePhotoMode ? Colors.grey : Colors.red,
+          backgroundColor: Colors.grey,
         ),
       );
     }
 
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.end,
       children: [
-        IconButton(
-          onPressed: () {},
-          color: Colors.white,
-          icon: const Icon(Icons.image),
-          iconSize: 30,
-        ),
+        (cameraController != null && cameraController.value.isRecordingVideo)
+            ? ElevatedButton(
+                onPressed: () {
+                  if (cameraController.value.isRecordingPaused) {
+                    _onResumeButtonPressed();
+                  } else {
+                    _onPauseButtonPressed();
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  shape: const CircleBorder(
+                    side: BorderSide(
+                      color: Colors.white,
+                      width: 2.0,
+                    ),
+                  ),
+                  padding: (cameraController.value.isRecordingPaused)
+                      ? const EdgeInsets.all(25)
+                      : const EdgeInsets.all(10),
+                  backgroundColor: (cameraController.value.isRecordingPaused)
+                      ? Colors.red
+                      : Colors.transparent,
+                ),
+                child: (cameraController.value.isRecordingPaused)
+                    ? null
+                    : const Center(
+                        child: Icon(
+                          Icons.pause,
+                          color: Colors.white,
+                          size: 30,
+                        ),
+                      ),
+              )
+            : IconButton(
+                onPressed: () {},
+                color: Colors.white,
+                icon: const Icon(Icons.image),
+                iconSize: 30,
+              ),
         const SizedBox(
           width: 30,
         ),
@@ -295,12 +541,16 @@ class _VideoRecordViewState extends State<VideoRecordView> {
         const SizedBox(
           width: 30,
         ),
-        IconButton(
-          icon: const Icon(Icons.flip_camera_ios),
-          color: Colors.white,
-          iconSize: 30,
-          onPressed: onChanged,
-        )
+        (cameraController != null && cameraController.value.isRecordingVideo)
+            ? SizedBox(
+                width: MediaQuery.sizeOf(context).width * 0.1,
+              )
+            : IconButton(
+                icon: const Icon(Icons.flip_camera_ios),
+                color: Colors.white,
+                iconSize: 30,
+                onPressed: onChanged,
+              )
       ],
     );
   }
@@ -365,6 +615,16 @@ class _VideoRecordViewState extends State<VideoRecordView> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        automaticallyImplyLeading: false,
+        elevation: 0,
+        systemOverlayStyle: const SystemUiOverlayStyle(
+          statusBarColor: Colors.transparent,
+          statusBarIconBrightness: Brightness.light,
+        ),
+      ),
       body: Column(
         children: [
           Expanded(
@@ -378,18 +638,26 @@ class _VideoRecordViewState extends State<VideoRecordView> {
                 width: MediaQuery.sizeOf(context).width,
                 child: Stack(
                   children: <Widget>[
-                    Positioned(
-                      top: 20,
-                      left: 10,
-                      child: IconButton(
-                        onPressed: () => Navigator.pop(context),
-                        color: Colors.white,
-                        iconSize: 30,
-                        icon: const Icon(Icons.keyboard_arrow_down),
-                      ),
-                    ),
                     Positioned.fill(
                       child: buildCameraPreviewWidget(),
+                    ),
+                    Positioned(
+                      top: 40,
+                      left: 20,
+                      child: GestureDetector(
+                        onTap: () {
+                          print("Tapped");
+                          Navigator.pop(context);
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.all(8.0),
+                          child: const Icon(
+                            Icons.keyboard_arrow_down,
+                            color: Colors.white,
+                            size: 30,
+                          ),
+                        ),
+                      ),
                     ),
                     Positioned(
                       bottom: 30,
